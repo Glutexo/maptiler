638cd6d8a37c97061c719b0bdee989fc7cbd914f
Glutexo <glutexo@icloud.com>
2025-06-02 16:10:49 +0200

    Use native defer instead of document event
    type="module" is deferred by default.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import * as maptilersdk from '@maptiler/sdk';
 import { init } from './npmmodule.js';
 
 window.maptilersdk = maptilersdk
-document.addEventListener('DOMContentLoaded', init);
+init();
