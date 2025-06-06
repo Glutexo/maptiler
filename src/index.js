a27c0e316c7559687c587b948ee5c7d88aad075a
Glutexo <glutexo@icloud.com>
2025-06-06 16:02:49 +0200

    Unify initialization scripts
    Named the initialization scripts after the pages they are used in. For
    Basic JavaScript, this means extracting it from in-line script
    tag. For NPM Module, defining the entry point in Webpack configuration file.

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import * as maptilersdk from '@maptiler/sdk';
-import '@maptiler/sdk/dist/maptiler-sdk.css';
-
-import './index.css';
-import init from './MapTiler.js';
-
-window.maptilersdk = maptilersdk;
-init();
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    entry: './src/Learn the basics NPM Module.js',
     module: {
         rules: [
             {
